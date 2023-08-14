5815b385409e2ed5debfb61598e222360d6267f1
taoxiyang <qiushui@wacai.com>
2023-08-14 14:14:47 +0800

    lab6

---
+++ proj2/byog/lab5/D2.java
@@ -0,0 +1,14 @@
+package byog.lab5;
+
+/**
+ * @author qiushui
+ * @Date 2023/8/12
+ */
+public class D2 {
+
+    public static void main(String[] args) {
+        int[][] d = new int[4][3];
+        System.out.println(d.length);
+        System.out.println(d[0].length);
+    }
+}
