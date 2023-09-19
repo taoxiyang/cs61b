8d1c864e77d4077dd6c3c09f876d903ffe17e204
taoxiyang <353570507@qq.com>
2023-09-19 20:37:30 +0800

    lab11

--- lab11/lab11/graphs/MazeBreadthFirstPaths.java
+++ lab11/lab11/graphs/MazeBreadthFirstPaths.java
@@ -1,5 +1,8 @@
 package lab11.graphs;
 
+import java.util.LinkedList;
+import java.util.Queue;
+
 /**
  *  @author Josh Hug
  */
@@ -10,20 +13,49 @@ public class MazeBreadthFirstPaths extends MazeExplorer {
     public boolean[] marked;
     */
 
+    private int s;
+    private int t;
+
+    private Queue<Integer> fringe = new LinkedList<>();
+
     public MazeBreadthFirstPaths(Maze m, int sourceX, int sourceY, int targetX, int targetY) {
         super(m);
         // Add more variables here!
+        s = maze.xyTo1D(sourceX, sourceY);
+        t = maze.xyTo1D(targetX, targetY);
+        distTo[s] = 0;
+        edgeTo[s] = s;
     }
 
     /** Conducts a breadth first search of the maze starting at the source. */
-    private void bfs() {
+    private void bfs(int v) {
         // TODO: Your code here. Don't forget to update distTo, edgeTo, and marked, as well as call announce()
+        fringe.add(v);
+
+        while (!fringe.isEmpty()){
+            int x = fringe.remove();
+            marked[x] = true;
+            announce();
+            if(x == t){
+                break;
+            }
+            for(int w : maze.adj(x)){
+                if(!marked[w]){
+                    marked[w] = true;
+                    edgeTo[w] = x;
+                    announce();
+                    distTo[w] = distTo[x] + 1;
+                    fringe.add(w);
+                }
+            }
+        }
     }
 
 
     @Override
     public void solve() {
         // bfs();
+        bfs(s);
     }
 }
 
--- lab11/lab11/graphs/MazeCycles.java
+++ lab11/lab11/graphs/MazeCycles.java
@@ -9,15 +9,57 @@ public class MazeCycles extends MazeExplorer {
     public int[] edgeTo;
     public boolean[] marked;
     */
+    private int s;
+
+    private boolean cycle = false;
+
 
     public MazeCycles(Maze m) {
         super(m);
+        s = maze.xyTo1D(1, 1);
+        distTo[s] = 0;
+        edgeTo[s] = Integer.MAX_VALUE;
     }
 
     @Override
     public void solve() {
         // TODO: Your code here!
+        dfs(s);
     }
+
+    private void dfs(int v){
+        if(cycle){
+            return;
+        }
+        marked[v] = true;
+        announce();
+        for (int w : maze.adj(v)) {
+            if (!marked[w]) {
+                edgeTo[w] = v;
+                announce();
+                distTo[w] = distTo[v] + 1;
+                dfs(w);
+                if (cycle) {
+                    return;
+                }
+            }else if(edgeTo[v] != w) {
+
+                int preNode = edgeTo[w];
+                while (preNode != Integer.MAX_VALUE){
+                    int pre = edgeTo[preNode];
+                    edgeTo[preNode] = Integer.MAX_VALUE;
+                    preNode = pre;
+                }
+
+                edgeTo[w] = v;
+                announce();
+                cycle = true;
+                return;
+            }
+        }
+    }
+
+
 
     // Helper methods go here
 }
